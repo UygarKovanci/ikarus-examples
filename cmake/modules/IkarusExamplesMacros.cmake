5cf4e6bca708001d4c1eec28d7563ded1d1ef725
AlexanderMueller <rathet@gmail.com>
2023-01-24 21:52:41 +0100

    test commenting deps

--- cmake/modules/IkarusExamplesMacros.cmake
+++ cmake/modules/IkarusExamplesMacros.cmake
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
 # File for module specific CMake tests.
-find_dependency(spdlog)
-find_dependency(Matplot++)
-find_dependency(Eigen3)
-find_dependency(autodiff)
+#find_dependency(spdlog)
+#find_dependency(Matplot++)
+#find_dependency(Eigen3)
+#find_dependency(autodiff)
